90184ca9b5c29c1f487488445a3ccbffac3437d5
jwalax5 <josh90810@gmail.com>
2024-05-23 01:17:34 +0900

    test if commit reflect on glitch directly

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>{/* Missing code */}</React.StrictMode>
+  <React.StrictMode>{/* Missing code testing */}</React.StrictMode>
 );
